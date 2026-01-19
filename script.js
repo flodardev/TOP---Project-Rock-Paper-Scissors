eac5adf591e8bf24eacf853f87da87b233da7bb6
flodardev <63950579+flodardev@users.noreply.github.com>
2026-01-19 15:46:38 +0800

    Added playGame() function

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 console.log("This is a rock paper scissors game");
 
 // Variables
-let round = 0; // To track how many rounds has been played
+let currentRound = 0; // To track how many rounds has been played
 let humanScore = 0; // To track human's score
 let computerScore = 0; // To track computer's score
 const ROCK = "Rock";
 const PAPER = "Paper";
 const SCISSOR = "Scissor"
+const MAX_ROUNDS = 4;
 
-playRound(getHumanChoice(), getComputerChoice());
-
+playGame();
 
 function getComputerChoice() {
     // Get a random number from 1 to 3
@@ -62,11 +62,11 @@ function playRound(humanChoice, computerChoice) {
         // If human chose Rock, check it against computer's choice
         case ROCK:
             if (computerChoice === PAPER) {
-                console.log("You lose!")
+                console.log("Computer won!")
                 // Computer won, increment computer's score
                 ++computerScore;
             } else if (computerChoice === SCISSOR) {
-                console.log("You win!")
+                console.log("Human won!")
                 // Human won, increment human's score
                 ++humanScore;
             } else {
@@ -77,13 +77,13 @@ function playRound(humanChoice, computerChoice) {
         // If human chose Paper, check it against computer's choice
         case PAPER:
             if (computerChoice === ROCK) {
-                console.log("You win!")
+                console.log("Human won!")
                 // Human won, increment human's score
                 ++humanScore;
             } else if (computerChoice === SCISSOR) {
-                console.log("You lose!")
+                console.log("Computer won!")
                 // Computer won, increment computer's score
-                ++computerScoreScore;
+                ++computerScore;
             } else {
                 console.log("It's a tie!")
             }
@@ -92,11 +92,11 @@ function playRound(humanChoice, computerChoice) {
         // If human chose Scissor, check it against computer's choice
         case SCISSOR:
             if (computerChoice === ROCK) {
-                console.log("You lose!")
+                console.log("Computer won!")
                 // Computer won, increment computer's score
                 ++computerScore;
             } else if (computerChoice === PAPER) {
-                console.log("You win!")
+                console.log("Human won!")
                 // Human won, increment human's score
                 ++humanScore;
             } else {
@@ -111,5 +111,20 @@ function playRound(humanChoice, computerChoice) {
 }
 
 function playGame() {
-    
+    while (currentRound <= MAX_ROUNDS) {
+        console.log(`Current round is ${currentRound}`)
+        playRound(getHumanChoice(), getComputerChoice());
+        ++currentRound;
+    }
+
+    // Declare an overall winnder
+    console.log("Human's final score = " + humanScore)
+    console.log("Computer's final score = " + computerScore)
+    if (humanScore > computerScore){
+        console.log("The winner is human.")
+    } else if (humanScore < computerScore) {
+        console.log("The winner is computer.")
+    } else {
+        console.log("It's a tie! No one won!")
+    }
 }
