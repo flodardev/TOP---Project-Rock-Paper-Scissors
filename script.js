8cb031ef4f0ee80f2c6be16e13135743f82dcbc8
flodardev <63950579+flodardev@users.noreply.github.com>
2026-01-24 13:07:31 +0800

    Complete UI, Function

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-console.log("This is a rock paper scissors game");
-
 // Variables
 let currentRound = 0; // To track how many rounds has been played
 let humanScore = 0; // To track human's score
 let computerScore = 0; // To track computer's score
+const COMPUTER = "Computer";
+const HUMAN = "Human";
+const TIE = "Tie"
 const ROCK = "Rock";
 const PAPER = "Paper";
 const SCISSOR = "Scissor"
-const MAX_ROUNDS = 4;
-
-playGame();
+const MAX_ROUNDS = 5;
 
 function getComputerChoice() {
     // Get a random number from 1 to 3
@@ -19,41 +18,15 @@ function getComputerChoice() {
     // 1 === ROCK, 2 === PAPER, 3 === SCISSOR
     switch(randomNumber) {
         case 1:
-            console.log(choiceText + ROCK)
             return ROCK;
         case 2:
-            console.log(choiceText + PAPER)
             return PAPER;
         case 3:
-            console.log(choiceText + SCISSOR)
             return SCISSOR;
         default:
             console.log("Error in getComputerChoice function")
             break;
     }
-}
-
-function getHumanChoice() {
-    // Prompt human for their choice, input number from 1 to 3
-    let humanChoice = +prompt("Choose a number from 1 to 3, 1 is for Rock, 2 is for Paper and 3 is for Scissor")
-    let choiceText = "Human's choice: "
-
-    // 1 === ROCK, 2 === PAPER, 3 === SCISSOR
-    switch(humanChoice){
-        case 1:
-            console.log(choiceText + ROCK)
-            return ROCK;
-        case 2:
-            console.log(choiceText + PAPER)
-            return PAPER;
-        case 3:
-            console.log(choiceText + SCISSOR)
-            return SCISSOR;
-        default:
-            console.log("Error in getHumanChoice function")
-            break;
-    }
-
 }
 
 function playRound(humanChoice, computerChoice) {
@@ -62,72 +35,80 @@ function playRound(humanChoice, computerChoice) {
         // If human chose Rock, check it against computer's choice
         case ROCK:
             if (computerChoice === PAPER) {
-                console.log("Computer won!")
-                // Computer won, increment computer's score
                 ++computerScore;
+                return COMPUTER;
             } else if (computerChoice === SCISSOR) {
-                console.log("Human won!")
-                // Human won, increment human's score
                 ++humanScore;
+                return HUMAN;
             } else {
-                console.log("It's a tie!")
+                return TIE;
             }
-            break;
         
         // If human chose Paper, check it against computer's choice
         case PAPER:
             if (computerChoice === ROCK) {
-                console.log("Human won!")
                 // Human won, increment human's score
                 ++humanScore;
+                return HUMAN;
             } else if (computerChoice === SCISSOR) {
-                console.log("Computer won!")
                 // Computer won, increment computer's score
                 ++computerScore;
+                return COMPUTER;
             } else {
-                console.log("It's a tie!")
+                return TIE;
             }
-            break;
 
         // If human chose Scissor, check it against computer's choice
         case SCISSOR:
             if (computerChoice === ROCK) {
-                console.log("Computer won!")
                 // Computer won, increment computer's score
                 ++computerScore;
+                return COMPUTER;
             } else if (computerChoice === PAPER) {
-                console.log("Human won!")
                 // Human won, increment human's score
                 ++humanScore;
+                return HUMAN;
             } else {
-                console.log("It's a tie!")
+                return TIE;
             }
-            break;
 
         default:
-            console.log("Error in playRound function")
+            console.log("Error in playRound function");
             break;
     }
 }
 
-function playGame() {
-    // while (currentRound <= MAX_ROUNDS) {
-    //     console.log(`Current round is ${currentRound}`)
-    //     playRound(getHumanChoice(), getComputerChoice());
-    //     ++currentRound;
-    // }
+function checkState() {
+    if (currentRound === MAX_ROUNDS){
+        document.body.innerHTML = ""
+        const body = document.querySelector("body");
+        let header = document.createElement("h1");
+        let para = document.createElement("h2");
+        let button = document.createElement("button");
+        button.textContent = "RESET";
+        button.addEventListener("click", x => window.location.reload());
 
-    // Declare an overall winnder
-    console.log("Human's final score = " + humanScore)
-    console.log("Computer's final score = " + computerScore)
-    if (humanScore > computerScore){
-        console.log("The winner is human.")
-    } else if (humanScore < computerScore) {
-        console.log("The winner is computer.")
-    } else {
-        console.log("It's a tie! No one won!")
-    }
-}
+        header.textContent = "GAME OVER!";
+
+        if (humanScore > computerScore) {
+            para.textContent = "Human won!";
+            body.append(header);
+            body.append(para);
+            body.append(button);
+
+        } else if (computerScore > humanScore) {
+            para.textContent = "Computer won!";
+            body.append(header);
+            body.append(para);
+            body.append(button);
+        
+        } else {
+            para.textContent = "It's a tie!";
+            body.append(header);
+            body.append(para);
+            body.append(button);
+        }
+    }}
 
 // BUTTONS
 const buttonRock = document.querySelector("#rock");
@@ -137,18 +118,102 @@ const buttonScissor = document.querySelector("#scissor")
 // Results DIV
 const divResults = document.querySelector(".results");
 
+// Announcement DIV
+const divAnnouncement = document.querySelector(".announcement");
+
+// Results Computer and Human score DIV
+const divHumanScore = document.querySelector(".human-score");
+const divComputerScore = document.querySelector(".computer-score");
+
+// Round DIV
+const divRound = document.querySelector(".current-round")
+
 // Rock
 buttonRock.addEventListener("click", e => {
-    divResults.append(e.target.textContent);
+    // Get winner and update DOM
+    e.preventDefault();
+
+    const winner = playRound(ROCK, getComputerChoice());
+    
+    switch(winner){
+        case HUMAN:
+            divAnnouncement.textContent = "Human won!";
+            break;
+        case COMPUTER:
+            divAnnouncement.textContent = "Computer won!";
+            break;
+        case TIE:
+            divAnnouncement.textContent = "It's a tie!";
+    }
+
+    // Update score
+    divHumanScore.textContent = humanScore;
+    divComputerScore.textContent = computerScore;
+
+    // Update round
+    currentRound++;
+    divRound.textContent = currentRound;
+
+    // Check current state
+    checkState();
 })
 
 // Paper
 buttonPaper.addEventListener("click", e => {
-    divResults.append(e.target.textContent);
-    console.log(e.target);
+    // Get winner and update DOM
+    e.preventDefault();
+
+    const winner = playRound(PAPER, getComputerChoice());
+    
+    switch(winner){
+        case HUMAN:
+            divAnnouncement.textContent = "Human won!";
+            break;
+        case COMPUTER:
+            divAnnouncement.textContent = "Computer won!";
+            break;
+        case TIE:
+            divAnnouncement.textContent = "It's a tie!";
+    }
+
+    // Update score UI
+    divHumanScore.textContent = humanScore;
+    divComputerScore.textContent = computerScore;
+
+    // Update round UI
+    currentRound++;
+    divRound.textContent = currentRound;
+
+    // Check current state
+    checkState();
 })
 
 // Scissor
 buttonScissor.addEventListener("click", e => {
-    console.log(e.target);
+    // Get winner and update DOM
+    e.preventDefault();
+
+    const winner = playRound(SCISSOR, getComputerChoice());
+    
+    switch(winner){
+        case HUMAN:
+            divAnnouncement.textContent = "Human won!";
+            break;
+        case COMPUTER:
+            divAnnouncement.textContent = "Computer won!";
+            break;
+        case TIE:
+            divAnnouncement.textContent = "It's a tie!";
+    }
+
+    // Update score UI
+    divHumanScore.textContent = humanScore;
+    divComputerScore.textContent = computerScore;
+
+    // Update round UI
+    currentRound++;
+    divRound.textContent = currentRound;
+
+    // Check current state
+    checkState();
 })
